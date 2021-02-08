895a1c8a2aab4d202e21a70c228b29a8d44ec0ea
Taner Selim <taner.selim@gameforge.com>
2021-02-08 23:49:42 +0100

    Initial windows dll creation

---
+++ engine/src/test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+#include <stdio.h>
+
+namespace Farscape {
+	void Print()
+	{
+		printf("Hello Farscape\n");
+	}
+}
---
+++ engine/src/test.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Farscape {
+	__declspec(dllexport) void Print();
+}
