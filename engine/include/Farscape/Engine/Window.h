198bb72ded0c24a6e289a247c6b01207409e548d
Taner Selim <taner.selim@gameforge.com>
2021-02-13 21:43:08 +0100

    Added generic platform agnostic window and assert capability

---
+++ engine/include/Farscape/Engine/Window.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "fspch.h"
+#include "Farscape/Engine/Core.h"
+#include "Farscape/Events/Event.h"
+
+namespace Farscape {
+
+	struct WindowProperties
+	{
+		std::string Title;
+		unsigned int Width;
+		unsigned int Height;
+
+		WindowProperties(
+			const std::string& title = "Farscape Engine",
+			unsigned int width = 1280,
+			unsigned int height = 780
+		)
+			: Title(title)
+			, Width(width)
+			, Height(height) {}
+	};
+
+	// A window for desktops
+	class FARSCAPE_API Window
+	{
+	public:
+		using EventCallbackFn = std::function<void(Event&)>;
+		virtual ~Window() {}
+		virtual void OnUpdate() = 0;
+
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		virtual void SetEventCallbacks(const EventCallbackFn& callback) = 0;
+		virtual void SetVSynch(bool enabled) = 0;
+		virtual void IsVSynch() const = 0;
+
+		// Creation function with reasonable defaults from the struct
+		// This should be implemented for every platform !!
+		static Window* Create(const WindowProperties& p = WindowProperties());
+	};
+
+}
