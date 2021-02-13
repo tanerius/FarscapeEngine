0da8b9acd78d2f54a09dda4d3e69acf4688c7fa3
Taner Selim <taner.selim@gameforge.com>
2021-02-13 21:42:50 +0100

    Added generic platform agnostic window and assert capability

--- engine/include/Farscape/Engine/Core.h
+++ engine/include/Farscape/Engine/Core.h
@@ -10,4 +10,12 @@
 #  define FARSCAPE_API
 #endif
 
+#if defined(FS_ENABLE_ASSERTS)
+	#define FS_ASSERT(x, ...) { if(!(x)) {FS_ERROR("Assertion failed! {0}", __VA_ARGS__); __debugbreak(); } }
+	#define FS_CORE_ASSERT(x, ...) { if(!(x)) {FS_CORE_ERROR("Assertion failed! {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define FS_ASSERT(x, ...) 
+	#define FS_CORE_ASSERT(x, ...) 
+#endif
+
 #define BIT(x) (1 << x)
