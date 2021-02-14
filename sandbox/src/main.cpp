5449386078f8277cd036757e9b3d8522496483be
Taner Selim <taner.selim@gameforge.com>
2021-02-14 22:12:04 +0100

    Basic layer test finished

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,11 +1,29 @@
 #include "Farscape/Farscape.h"
 
+// create an example layer
+class ExampleLayer : public Farscape::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example") {}
+
+	void OnUpdate() override
+	{
+		FS_INFO("ExampleLayer::Update");
+	}
+
+	void OnEvent(Farscape::Event& event) override
+	{
+		FS_TRACE("{0}", event);
+	}
+};
+
 class Sandbox : public Farscape::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox()
