ed2cc9d29fc8514774cbb364f585c8674615dd8d
Taner Selim <taner.selim@gameforge.com>
2021-02-18 14:44:33 +0100

    Shader abstraction

---
+++ Farscape/src/include/Renderer/Shader.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+
+namespace Farscape {
+
+	class Shader
+	{
+	public:
+		Shader(std::string& vertexSrc, std::string& fragmentSrc);
+		~Shader();
+
+		void Bind() const;
+		void Unbind() const;
+	private:
+		uint32_t m_RendererID;
+	};
+
+}
