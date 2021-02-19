f98daf6f01ca27e46e5615735efce082690693c7
Taner Selim <taner.selim@gameforge.com>
2021-02-19 01:44:37 +0100

    [WIP] buffer strides or offsets are incorrect ..check tomorrow

--- Farscape/src/include/Renderer/Buffer.h
+++ Farscape/src/include/Renderer/Buffer.h
@@ -98,6 +98,8 @@ namespace Farscape {
 		
 		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
 		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
 	private:
 		void CalculateOffsetsAndStride()
 		{
--- Farscape/src/private/Application.cpp
+++ Farscape/src/private/Application.cpp
@@ -49,29 +49,37 @@ namespace Farscape {
 
 		
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		BufferLayout layout = {
-			{ShaderDataType::Float3, "a_Position"}
-		};
+		// put this in a separate scope so it gets destroyed
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3, "a_Position"},
+				{ShaderDataType::Float4, "a_Color"}
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
 
 		uint32_t index = 0;
-		for (auto& e : layout)
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& e : layout)
 		{
+			BufferElement x = e;
 			glEnableVertexAttribArray(index);
 			glVertexAttribPointer(
 				index, 
 				e.GetComponentCount(), 
 				ShaderDataTypeToOpenGLType(e.type), 
 				e.normalized ? GL_TRUE : GL_FALSE, 
-				layout.GetStride(), 
-				(const void*)((uint64_t)e.offset));
+				layout.GetStride(),
+				(const void*)((uint64_t)e.offset)); // TODO: I THINK THIS IS A PROBLEM!!! BUT LETS SEE
 			index++;
 		}
 
@@ -87,8 +95,10 @@ namespace Farscape {
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 			
 			void main()
 			{
@@ -105,10 +115,12 @@ namespace Farscape {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
-			
+			in vec4 v_Color;			
+
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 
 	)";
