298e816cf80ebb331f927fa889a4e9a000485bef
Taner Selim <tanerius@gmail.com>
2021-03-01 23:09:09 +0100

    Assert the minimum OpenGL version

--- Farscape/platform/Windows/OpenGLContext.cpp
+++ Farscape/platform/Windows/OpenGLContext.cpp
@@ -24,6 +24,15 @@ namespace Farscape {
         FS_CORE_INFO("Vendor: {0} ", glGetString(GL_VENDOR));
         FS_CORE_INFO("Renderer: {0} ", glGetString(GL_RENDERER));
         FS_CORE_INFO("Version: {0} ", glGetString(GL_VERSION));
+
+#ifdef FS_ENABLE_ASSERTS
+        int versionMajor;
+        int versionMinor;
+        glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+        glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+        FS_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Minimum OpenGL version >= 4.5!");
+#endif
     }
 
     void OpenGLContext::SwapBuffers()
