ef396da789750c51b9a7f7bf32202b4b1982cc16
Taner Selim <tanerius@gmail.com>
2021-03-21 20:23:31 +0100

    entt test

--- Tests/Farscape/src/Component_test.cpp
+++ Tests/Farscape/src/Component_test.cpp
@@ -1,6 +1,8 @@
 #include "Farscape.h"
+#include <entt/entt.hpp>
 
 #include <gtest/gtest.h>
+
 
 
 
@@ -10,4 +12,25 @@ TEST(ComponentTest, TagComponent)
     comp.Tag = "Test";
 
     ASSERT_STREQ("Test", ((std::string)comp).c_str());
+}
+
+TEST(EcsTest, Entt)
+{
+    struct position {
+        float x;
+        float y;
+    };
+
+    struct velocity {
+        float dx;
+        float dy;
+    };
+
+    entt::registry reg;
+
+    for (auto i = 0u; i < 10u; ++i) {
+        const auto entity = reg.create();
+        reg.emplace<position>(entity, i * 1.f, i * 1.f);
+        if (i % 2 == 0) { reg.emplace<velocity>(entity, i * .1f, i * .1f); }
+    }
 }
