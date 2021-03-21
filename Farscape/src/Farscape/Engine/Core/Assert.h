fa4619e328523aae21e92bdb5d2a133ed83ac35e
Taner Selim <tanerius@gmail.com>
2021-03-21 17:56:46 +0100

    Using more streamlined pletform detection now also tagged 0.0.2

--- Farscape/src/Farscape/Engine/Core/Assert.h
+++ Farscape/src/Farscape/Engine/Core/Assert.h
@@ -6,7 +6,7 @@
 #endif
 
 #if defined(FS_ENABLE_ASSERTS)
-#if defined(_WIN64)
+#if defined(FS_WIN64)
 #define FS_ASSERT_NO_MESSAGE(condition) { if(!(condition)) { FS_ERROR("Assertion Failed"); __debugbreak(); } }
 #define FS_ASSERT_MESSAGE(condition, ...) { if(!(condition)) { FS_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 
@@ -15,7 +15,7 @@
 
 #define FS_ASSERT(...) FS_EXPAND_VARGS( FS_GET_ASSERT_MACRO(__VA_ARGS__)(__VA_ARGS__) )
 #define FS_CORE_ASSERT(...) FS_EXPAND_VARGS( FS_GET_ASSERT_MACRO(__VA_ARGS__)(__VA_ARGS__) )
-#elif FS_PLATFORM == MacOS
+#elif defined(FS_APPLE)
 #define FS_ASSERT(x, ...) //{ if(!(x)) {FS_ERROR("Assertion failed! {0}", __VA_ARGS__); __asm__("int $3"); } }
 #define FS_CORE_ASSERT(x, ...) //{FS_CORE_ERROR("Assertion failed! {0}", __VA_ARGS__); __asm__("int $3"); } }
 #endif
--- Farscape/src/Farscape/Engine/Core/Core.h
+++ Farscape/src/Farscape/Engine/Core/Core.h
@@ -3,38 +3,21 @@
 #include <memory>
 #include "ManagedPointers.h"
 
-#if defined(_WIN32) // Any windows 32 or 64 bit
-#if defined(_WIN64) //Windows 64bit
-#define FS_PLATFORM FS_PLATFORM_WINDOWS
-#if !defined(FARSCAPE_STATIC)
-#if defined(FARSCAPE_BUILD_EXPORTS)
-#define FARSCAPE_API __declspec(dllexport)
+#if defined(FS_WIN64) // Any windows 32 or 64 bit
+    #if !defined(FARSCAPE_STATIC)
+        #if defined(FARSCAPE_BUILD_EXPORTS)
+            #define FARSCAPE_API __declspec(dllexport)
+        #else
+            #define FARSCAPE_API __declspec(dllimport)
+        #endif
+    #else
+        #define FARSCAPE_API
+    #endif
+#elif defined(FS_APPLE)
+    #include <TargetConditionals.h>
+    #define FARSCAPE_API
 #else
-#define FARSCAPE_API __declspec(dllimport)
-#endif
-#else
-#define FARSCAPE_API
-#endif
-#else
-#error "Win x86 is not supported!"
-#endif
-#elif defined(__APPLE__) || defined(__MACH__)
-#include <TargetConditionals.h>
-#define FARSCAPE_API
-
-#if TARGET_IPHONE_SIMULATOR == 1
-#define FS_PLATFORM IOS_Sim
-#error "IOS simulator is not supported!"
-#elif TARGET_OS_IPHONE == 1
-#define FS_PLATFORM IOS
-#error "IOS is not supported!"
-#elif TARGET_OS_MAC == 1
-#define FS_PLATFORM MacOS
-// #error "MacOS is not supported!"
-#else
-#define FARSCAPE_API __declspec(dllimport)
-#error "Unknown Apple platform!"
-#endif
+    #error "Unknown Apple platform!"
 #endif
 
 namespace Farscape {
@@ -43,10 +26,6 @@ namespace Farscape {
     void ShutdownEngine();
 
 }
-
-//#ifndef FS_PLATFORM_WINDOWS
-//#error Farscape only supports Windows!
-//#endif
 
 // __VA_ARGS__ expansion to get past MSVC "bug"
 #define FS_EXPAND_VARGS(x) x
@@ -71,16 +50,6 @@ namespace Farscape {
     {
         return std::make_unique<T>(std::forward<Args>(args)...);
     }
-
-    /*
-    template<typename T>
-    using Ref = std::shared_ptr<T>;
-    template<typename T, typename ... Args>
-    constexpr Ref<T> CreateRef(Args&& ... args)
-    {
-        return std::make_shared<T>(std::forward<Args>(args)...);
-    }
-    */
 
     using byte = uint8_t;
 }
