6080bcfe0e5419ee664452055ab0e20fc9c53ecb
Taner Selim <tanerius@gmail.com>
2021-04-05 20:12:14 +0200

    Added ScriptCore to enable accessing Farscape stuff with C# scripting

---
+++ SampleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SampleApp")]
+[assembly: AssemblyDescription("Test integration with Farscape Engine")]
+
+#if (DEBUG)
+
+[assembly: AssemblyConfiguration("Debug")]
+
+#else
+
+[assembly: AssemblyConfiguration("Release")]
+
+#endif
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FarscapeEngine")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("AD535DCE-1B80-4390-8076-2EE027CCD2EE")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
